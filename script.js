24ad75bb170585dd5a2796187f91381cc53aa06a
Vladimir Grekov <grekov.vo@yandex.ru>
2023-04-29 17:33:41 +0300

    Сдача проекта на проверку

---
+++ script.js
@@ -0,0 +1,40 @@
+let popup = document.querySelector('.popup')
+let edit = document.querySelector('.button__edit');
+let InfoName = document.querySelector('.profile__info_name');
+let InfoJob = document.querySelector('.profile__info_job');
+let EditInfoName = document.querySelector('.edit_form__name');
+let EditInfoJob = document.querySelector('.edit_form__job');
+let remove = document.querySelector('.popup__button_close');
+let like = document.querySelector('.elements__element_button')
+
+function editInfo() {
+  popup.classList.add('popup__opened');
+  EditInfoName.value = InfoName.textContent;
+  EditInfoJob.value = InfoJob.textContent;
+};
+
+function RemoveEditInfo() {
+  popup.classList.remove('popup__opened');
+  renderAdded()
+};
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  InfoName.textContent = EditInfoName.value;
+  InfoJob.textContent = EditInfoJob.value;
+  RemoveEditInfo();
+};
+
+function changeImageLike() {
+  if  (document.getElementById('IconLike').src = './images/Like.svg'){
+  document.getElementById('IconLike').src = './images/Union.svg';}
+  else
+  {if  (document.getElementById('IconLike').src = './images/Union.svg'){
+    document.getElementById('IconLike').src = './images/Like.svg';}};
+  renderAdded()
+};
+
+edit.addEventListener('click', editInfo);
+remove.addEventListener('click', RemoveEditInfo); 
+popup.addEventListener('submit', handleFormSubmit);
+like.addEventListener('click', changeImageLike)

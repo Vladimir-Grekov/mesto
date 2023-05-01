47a486fa7c33e8cc8f8323c1a4cb50d26261c174
Vladimir Grekov <grekov.vo@yandex.ru>
2023-05-01 12:19:55 +0300

    оптимизация по БЭМ

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let popup = document.querySelector('.popup')
 let edit = document.querySelector('.button__edit');
-let InfoName = document.querySelector('.profile__info_name');
-let InfoJob = document.querySelector('.profile__info_job');
-let EditInfoName = document.querySelector('.edit_form__name');
-let EditInfoJob = document.querySelector('.edit_form__job');
-let remove = document.querySelector('.popup__button_close');
-let like = document.querySelector('.elements__element_button')
+let InfoName = document.querySelector('.profile__info-name');
+let InfoJob = document.querySelector('.profile__info-job');
+let EditInfoName = document.querySelector('.edit-form__name');
+let EditInfoJob = document.querySelector('.edit-form__job');
+let remove = document.querySelector('.button__close');
+let like = document.querySelector('.button__like-icon');
 
 function editInfo() {
   popup.classList.add('popup__opened');
@@ -25,7 +25,16 @@ function handleFormSubmit (evt) {
   RemoveEditInfo();
 };
 
+function changeImageLike() {
+if  (like.src = './images/Like.svg'){
+like.src = './images/Union.svg';}
+else
+{if  (like.src = './images/Union.svg'){
+like.src = './images/Like.svg';}};
+renderAdded()
+};
+
 edit.addEventListener('click', editInfo);
 remove.addEventListener('click', RemoveEditInfo); 
 popup.addEventListener('submit', handleFormSubmit);
-like.addEventListener('click', changeImageLike)
+like.addEventListener('click', changeImageLike);

e1ec2106feda11e262d5dfe24fc8e662d5f92358
Vladimir Grekov <grekov.vo@yandex.ru>
2023-05-30 13:13:34 +0300

    Вторая итерация

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 let popup = document.querySelector('.popup')
-let editForm = document.querySelector('.popup__container')
-let edit = document.querySelector('.button_edit');
+let editForm = document.querySelector('.edit-form')
+let edit = document.querySelector('.profile__info_button_edit');
 let infoName = document.querySelector('.profile__info-name');
 let infoJob = document.querySelector('.profile__info-job');
 let editInfoName = document.querySelector('.edit-form__input_edit_name');
 let editInfoJob = document.querySelector('.edit-form__input_edit_job');
-let remove = document.querySelector('.button_close');
+let remove = document.querySelector('.popup__container_button_close');
 
 function editInfo() {
   popup.classList.add('popup_opened');
